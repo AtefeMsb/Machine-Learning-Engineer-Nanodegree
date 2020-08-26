e58ab14747f45f7ff80c864b7e755c633a600d9d
Atefe <atefe.mosayebi@gmail.com>
2020-08-26 16:11:06 -0700

    added Ned Batchelder testing tutorial files

---
+++ Ned-Batchelder-Testing/porttest3.py
@@ -0,0 +1,14 @@
+# porttest3.py
+from portfolio1 import Portfolio
+
+p = Portfolio()
+print("Empty portfolio cost: {}, should be 0.0".format(p.cost()))
+assert p.cost() == 0.0
+
+p.buy("IBM", 100, 176.48)
+print("With 100 IBM @ 176.48: {}, should be 17648.0".format(p.cost()))
+assert p.cost() == 17648.0
+
+p.buy("HPQ", 100, 36.15)
+print("With 100 HPQ @ 36.15: {}, should be 21263.0".format(p.cost()))
+assert p.cost() == 21263.0
